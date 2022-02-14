04421624899290d39498c36e80f1adf586012682
PraktikumJava <ragozina.anastasiia@yandex.ru>
2022-02-14 08:38:08 +0300

    fix: remove additional annotation

--- src/main/java/ru/yandex/practicum/Point.java
+++ src/main/java/ru/yandex/practicum/Point.java
@@ -2,11 +2,9 @@ package ru.yandex.practicum;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class Point {
     private int x;
     private int y;
